7e0d095def056514ea2fdb23f76c4bb571b847ba
Myungsun Kim <rlaaudtjs@gist.ac.kr>
2020-12-16 16:36:54 +0900

    change train model from lstm to rf

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 
 train = pd.read_csv('data/train/train.csv')
-test_sample = pd.read_csv('data/test/0.csv')
 submission = pd.read_csv('data/sample_submission.csv')
 submission.set_index('id',inplace=True)
+
+train['D_sum'] = train['DHI'] + train['DNI']
 
 #%% pre-processing
 def multivariate_data(dataset, target, start_index, end_index, history_size,
@@ -28,10 +29,11 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
         data = data.reshape(-1,history_size,1)
     return data, labels
 
-used_features =['TARGET']
+used_features =[ 'DHI', 'DNI', 'WS', 'RH', 'T', 'D_sum', 'TARGET']
+# used_features =['D_sum', 'TARGET']
 dataset = train.loc[:,used_features].values
 FEATURES = len(used_features)
-past_history = 48*3
+past_history = 48*7
 future_target = 48*2
 STEP = 1
 
@@ -46,113 +48,92 @@ X_train, y_train = X_train[:TRAIN_SPLIT], y_train[:TRAIN_SPLIT]
 
 test = []
 for i in range(81):
+    data = []
     tmp = pd.read_csv(f'data/test/{i}.csv')
+    tmp['D_sum'] = tmp['DHI'] + tmp['DNI']
     tmp = tmp.loc[:, used_features].values
     tmp = tmp[-past_history:,:]
-    tmp = tmp.reshape(-1, past_history, FEATURES)
-    test.append(tmp)
+    data.append(tmp)
+    data = np.array(data)
+    if FEATURES == 1:
+        data = data.reshape(-1, past_history, 1)
+    test.append(data)
 test = np.concatenate(test, axis=0)
 
-#%% train
-from tensorflow.keras.models import Sequential
-import tensorflow as tf
-from tensorflow.keras import initializers
-import tensorflow.keras.backend as K
-from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
-from tensorflow.keras.optimizers import Adam
+X_train = X_train.transpose((0,2,1))
+X_val = X_val.transpose((0,2,1))
+test = test.transpose((0,2,1))
 
-def custom_loss(q = 0.1):
-    def pinball_loss(y_true, y_pred):
-        idx1 = y_true >= y_pred
-        idx2 = y_true < y_pred
+X_train = X_train.reshape(-1,past_history*FEATURES)
+X_val = X_val.reshape(-1,past_history*FEATURES)
+test = test.reshape(-1,past_history*FEATURES)
 
-        y_true_1, y_pred_1 = K.flatten(y_true[idx1]),K.flatten(y_pred[idx1])
-        y_true_2, y_pred_2 = K.flatten(y_true[idx2]),K.flatten(y_pred[idx2])
+#%% rf model
+X_train = X_train.transpose((0,2,1))
+X_val = X_val.transpose((0,2,1))
+test = test.transpose((0,2,1))
 
-        loss_1 = (y_true_1-y_pred_1)*q
-        loss_2 = (y_pred_2 - y_true_2) * (1 - q)
+X_train = X_train.reshape(-1,past_history*FEATURES)
+X_val = X_val.reshape(-1,past_history*FEATURES)
+test = test.reshape(-1,past_history*FEATURES)
 
-        loss = K.concatenate([loss_1, loss_2])
-        return K.mean(loss)
-    return pinball_loss
+from sklearn import ensemble
+N_ESTIMATORS = 1000
+rf = ensemble.RandomForestRegressor(n_estimators=N_ESTIMATORS,
+                                    min_samples_leaf=1, random_state=3,
+                                    verbose=True,
+                                    n_jobs=-1)  # Use maximum number of cores.
 
-# model train
-model = Sequential([
-    tf.keras.layers.LSTM(past_history,
-                         return_sequences=False,
-                         input_shape=X_train.shape[-2:],
-                         kernel_initializer=initializers.he_normal()),
-    tf.keras.layers.Dropout(.1),
-    tf.keras.layers.Dense(future_target*3,
-                          kernel_initializer=initializers.he_normal()),
-    tf.keras.layers.Dropout(.1),
-    tf.keras.layers.Dense(future_target,
-                          kernel_initializer=initializers.he_normal()),
-])
-es = EarlyStopping(
-    monitor='val_loss',
-    min_delta=0,
-    patience=15,
-    verbose=0,
-    mode='auto',
-    baseline=None,
-    restore_best_weights=True
-)
+rf.fit(X_train, y_train)
 
-def scheduler(epoch, lr):
-  if epoch < 30:
-    return lr
-  else:
-    return lr * tf.math.exp(-0.1)
-
-lr = LearningRateScheduler(scheduler)
-
-#%% train model
-EPOCHS = 100
-BATCH_SIZES = 256
+## val
+rf_preds = []
+for estimator in rf.estimators_:
+    rf_preds.append(estimator.predict(X_val))
+rf_preds = np.array(rf_preds)
 
 val_preds_df = pd.DataFrame(columns=list(submission.columns) + ['true'],
                            data=np.zeros((y_val.shape[0] * future_target,10)))
 val_preds_df['true'] = np.ravel(y_val)
-optimizer = Adam(learning_rate=0.01)
 
 for i, q in enumerate(np.arange(0.1, 1, 0.1)):
-    model.compile(optimizer=optimizer, loss=custom_loss(q=q), metrics=[custom_loss(q=q)])
-    history = model.fit(X_train, y_train, epochs=EPOCHS,validation_data=(X_val, y_val), batch_size=BATCH_SIZES,
-                        callbacks=[lr, es], verbose=2)
-    tf.keras.backend.clear_session()
-
-    # validation
-    val_pred = model.predict(X_val)
+    val_pred = np.percentile(rf_preds, q * 100, axis=0)
     val_preds_df.iloc[:,i] = np.ravel(val_pred)
 
-    # test pred
-    y_pred = model.predict(test)
-    submission.iloc[:,i] = np.ravel(y_pred)
+## test
+rf_preds = []
+for estimator in rf.estimators_:
+    rf_preds.append(estimator.predict(test))
+rf_preds = np.array(rf_preds)
+
+for i, q in enumerate(np.arange(0.1, 1, 0.1)):
+    y_pred = np.percentile(rf_preds, q * 100, axis=0)
+    submission.iloc[:, i] = np.ravel(y_pred)
+
+submission.to_csv('submit/submit_11.csv')
+# save corresponding val result
+val_preds_df.to_csv('val/val_11.csv')
 
 #%% evaluation
-def pinball_loss(y_true, y_pred, q=0.5):
-    idx1 = y_true >= y_pred
-    idx2 = y_true < y_pred
+import tensorflow.keras.backend as K
+def pinball(tao, y_true, y_pred):
+    pin = K.mean(K.maximum(y_true - y_pred, 0) * tao +
+                 K.maximum(y_pred - y_true, 0) * (1 - tao))
+    return pin
 
-    y_true_1, y_pred_1 = np.ravel(y_true[idx1]),np.ravel(y_pred[idx1])
-    y_true_2, y_pred_2 = np.ravel(y_true[idx2]),np.ravel(y_pred[idx2])
-
-    loss_1 = (y_true_1 - y_pred_1)*q
-    loss_2 = (y_pred_2 - y_true_2) * (1 - q)
-
-    loss = np.concatenate([loss_1, loss_2])
-    return np.mean(loss)
+import matplotlib.pyplot as plt
+val_preds_df = pd.read_csv('val/val_11.csv')
+val_preds_df.set_index('Unnamed: 0',inplace=True)
 
 val_results = []
 val_preds_df[val_preds_df<0] = 0
 for q in np.arange(0.1,1,0.1):
-    val = pinball_loss(val_preds_df['true'], val_preds_df['q_'+str(q)[:3]], q=q)
+    val = pinball(q,val_preds_df['true'], val_preds_df['q_'+str(q)[:3]])
     val_results.append(val)
 print(np.mean(val_results, axis=0))
 
-#%% minus to 0
-submission[submission<0] = 0
-submission.to_csv('submit/submit_1.csv')
-# save corresponding val result
-val_preds_df.to_csv('val/val_1.csv')
+plt.plot(val_preds_df.iloc[:48,9],'r',label='True')
+for i in range(9):
+    plt.plot(val_preds_df.iloc[:48,i],'x',label=val_preds_df.columns[i])
+plt.legend()
+plt.show()
