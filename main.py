0445a3a1f2c032d70f21f993217fbaf0937c496f
Myungsun Kim <rlaaudtjs@gist.ac.kr>
2020-12-24 23:40:07 +0900

    add ensemble

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import pandas as pd
 import numpy as np
+import pickle
+import tensorflow as tf
+import os
+# Seed value (can actually be different for each attribution step)
+seed_value= 0
 
+# 1. Set `PYTHONHASHSEED` environment variable at a fixed value
+os.environ['PYTHONHASHSEED']=str(seed_value)
+
+# 2. Set `python` built-in pseudo-random generator at a fixed value
+import random
+random.seed(seed_value)
+
+# 3. Set `numpy` pseudo-random generator at a fixed value
+np.random.seed(seed_value)
+
+# 4. Set `tensorflow` pseudo-random generator at a fixed value
+tf.random.set_seed(seed_value) # tensorflow 2.x
+
+def pinball(y_true, y_pred, q):
+    pin = K.mean(K.maximum(y_true - y_pred, 0) * q +
+                 K.maximum(y_pred - y_true, 0) * (1 - q))
+    return pin
+
+def load_obj(name):
+    with open('tune_results/' + name + '.pkl', 'rb') as f:
+        trials = pickle.load(f)
+        trials = sorted(trials, key=lambda k: k['loss'])
+        return trials
+
+# load tuning results
+# rf_param = load_obj('rf_3')[0]['params']
+dnn_param = load_obj('dnn_3')[0]['params']
+weight = [1,0]
+lags = 3
+
+# load dataset
 train = pd.read_csv('data/train/train.csv')
 submission = pd.read_csv('data/sample_submission.csv')
 submission.set_index('id',inplace=True)
@@ -8,7 +44,7 @@ submission.set_index('id',inplace=True)
 train['D_sum'] = train['DHI'] + train['DNI']
 
 # save val result format
-save_num = 20
+save_num = 21
 
 #%% pre-processing
 def multivariate_data(dataset, target, start_index, end_index, history_size,
@@ -19,14 +55,17 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
     if end_index is None:
         end_index = len(dataset) - target_size
     for i in range(start_index, end_index, 48):
-        indices = range(i-history_size, i, step)
-        data.append(np.ravel(dataset[indices].T))
+        indices = range(i - history_size, i, step)
+        data.append(dataset[indices])
         if single_step:
-            labels.append(target[i+target_size])
+            labels.append(target[i + target_size])
         else:
-          labels.append(target[i:i+target_size])
+            labels.append(target[i:i + target_size])
     data = np.array(data)
     labels = np.array(labels)
+    if FEATURES == 1:
+        # univariate
+        data = data.reshape(-1, history_size, 1)
     return data, labels
 
 x_col =['DHI','DNI','D_sum','WS','RH','T','TARGET']
@@ -36,7 +75,7 @@ dataset = train.loc[:,x_col].values
 label = np.ravel(train.loc[:,y_col].values)
 
 FEATURES = len(x_col)
-past_history = 48 * 3
+past_history = 48 * lags
 future_target = 48 * 2
 
 ### transform train
@@ -57,64 +96,97 @@ for i in range(81):
     test.append(data)
 test = np.concatenate(test, axis=0)
 
+import tensorflow.keras.backend as K
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+X_train, X_val, y_train, y_val = train_test_split(train_data, train_label, test_size = 0.2, random_state = 42)
+
+# pre-precessing
+X_train = X_train.transpose((0, 2, 1))
+X_val = X_val.transpose((0, 2, 1))
+X_train = X_train.reshape(-1, past_history * FEATURES)
+X_val = X_val.reshape(-1, past_history * FEATURES)
+
 #%% rf model
-def pinball_loss(q,y_true, y_pred):
-    idx1 = y_true >= y_pred
-    idx2 = y_true < y_pred
+rf = RandomForestRegressor(**rf_param)
+rf.fit(X_train, y_train)
 
-    y_true_1, y_pred_1 = np.ravel(y_true[idx1]),np.ravel(y_pred[idx1])
-    y_true_2, y_pred_2 = np.ravel(y_true[idx2]),np.ravel(y_pred[idx2])
+rf_preds = []
+for estimator in rf.estimators_:
+    rf_preds.append(estimator.predict(X_val))
+rf_preds = np.array(rf_preds)
 
-    loss_1 = (y_true_1 - y_pred_1)*q
-    loss_2 = (y_pred_2 - y_true_2) * (1 - q)
+val_results = pd.DataFrame(columns=['rf','dnn'], data = np.zeros((9,2)))
+rf_preds_df = pd.DataFrame(columns=list(submission.columns) + ['true'],
+                           data=np.zeros((y_val.shape[0] * future_target,9)))
+for i, q in enumerate(np.arange(0.1, 1, 0.1)):
+    val_pred = np.percentile(rf_preds, q * 100, axis=0)
+    val = pinball(y_val, val_pred, q)
+    val_results.loc[i,'rf'] = val
+    rf_preds_df.iloc[:, i] = np.ravel(val_pred)
 
-    loss = np.concatenate([loss_1, loss_2])
-    return np.mean(loss)
+rf_preds_df['true'] = np.ravel(y_val)
+print('rf')
+print(np.mean(val_results['rf']))
 
-from sklearn import ensemble
-N_ESTIMATORS = 1000
-rf = ensemble.RandomForestRegressor(n_estimators=N_ESTIMATORS,
-                                    random_state=0,
-                                    max_depth = 5,
-                                    max_features=1,
-                                    criterion='mae',
-                                    verbose=True,
-                                    n_jobs=-1)  # Use maximum number of cores.
+#%% dnn model
+def custom_loss(q):
+    def pinball(y_true, y_pred):
+        pin = K.mean(K.maximum(y_true - y_pred, 0) * q +
+                     K.maximum(y_pred - y_true, 0) * (1 - q))
+        return pin
+    return pinball
 
-## LOOCV
-sample_num = train_data.shape[0]
-loss_list = np.zeros([sample_num, 1])
-val_pred = [np.zeros(train_label.shape) for _ in range(9)]
-from sklearn.model_selection import KFold
-kf = KFold(n_splits=5, shuffle=True)
+def dnn_val(X_train, y_train, X_val, y_val, params, q):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(params['h1'], activation='relu', input_shape=(X_train.shape[1],)))
+    model.add(tf.keras.layers.Dense(params['h2'], activation='relu'))
+    model.add(tf.keras.layers.Dense(params['h1'], activation='relu'))
+    model.add(tf.keras.layers.Dense(future_target))
 
-from tqdm import tqdm
-for train_index, test_index in tqdm(kf.split(train_data)):  # cross validation
-    X_train, X_test = train_data[train_index], train_data[test_index]
-    y_train, y_test = train_label[train_index], train_label[test_index]
+    optimizer = tf.keras.optimizers.Adam(params['lr'])
+    es = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=10,
+        verbose=0,
+        mode='auto',
+        restore_best_weights=True
+    )
+    model.compile(optimizer=optimizer, loss=custom_loss(q=q), metrics=[custom_loss(q=q)])
+    history = model.fit(X_train, y_train, epochs=params['EPOCH'], verbose=1, batch_size=params['BATCH_SIZE'],
+                        validation_data=(X_val, y_val), callbacks=[es])
+    y_pred = model.predict(X_val)
+    # validation
+    return np.min(history.history['val_loss']), y_pred, model
 
-    rf.fit(X_train, y_train)
+dnn_preds_df = pd.DataFrame(columns=list(submission.columns) + ['true'],
+                           data=np.zeros((y_val.shape[0] * future_target,10)))
 
-    rf_preds = []
-    for estimator in rf.estimators_:
-        rf_preds.append(estimator.predict(X_test))
-    rf_preds = np.array(rf_preds)
+for i, q in enumerate(np.arange(0.1, 1, 0.1)):
+    g = tf.Graph()
+    with g.as_default():
+        val, val_pred, _ = dnn_val(X_train, y_train, X_val, y_val, dnn_param, q)
+        val_results.loc[i,'dnn'] = val
+        dnn_preds_df.iloc[:, i] = np.ravel(val_pred)
+    tf.keras.backend.clear_session()
 
-    for i, q in enumerate(np.arange(0.1, 1, 0.1)):
-        val_pred[i][test_index] = np.percentile(rf_preds, q * 100, axis=0)
+dnn_preds_df['true'] = np.ravel(y_val)
 
-# val result dictionary and loss
-val_preds_df = pd.DataFrame(columns=list(submission.columns) + ['true'],
-                           data=np.zeros((train_label.shape[0] * future_target,10)))
-val_preds_df['true'] = np.ravel(train_label)
+print('dnn')
+print(np.mean(val_results['dnn']))
+
+#%% ensemble 성능 검증
 losses = []
 for i, q in enumerate(np.arange(0.1, 1, 0.1)):
-    val_preds_df.iloc[:, i] = np.ravel(val_pred[i])
-    losses.append(pinball_loss(q, np.ravel(train_label), np.ravel(val_pred[i])))
-loss = np.mean(losses, axis=0)
-print(loss)
+    pred = rf_preds_df.iloc[:,i] * weight[0] + dnn_preds_df.iloc[:,i] * weight[1]
+    loss = pinball(rf_preds_df['true'].values, pred, q)
+    losses.append(loss)
+print(np.mean(losses))
 
 #%% test == 전체 데이터셋 사용
+# rf
+rf = RandomForestRegressor(**rf_param)
 rf.fit(train_data, train_label)
 
 rf_preds = []
@@ -122,19 +194,41 @@ for estimator in rf.estimators_:
     rf_preds.append(estimator.predict(test))
 rf_preds = np.array(rf_preds)
 
+rf_preds_test = []
 for i, q in enumerate(np.arange(0.1, 1, 0.1)):
     y_pred = np.percentile(rf_preds, q * 100, axis=0)
-    submission.iloc[:, i] = np.ravel(y_pred)
+    rf_preds_test.append(np.ravel(y_pred))
+
+# dnn
+dnn_preds_test = []
+for i, q in enumerate(np.arange(0.1, 1, 0.1)):
+    g = tf.Graph()
+    with g.as_default():
+        _, _, model = dnn_val(X_train, y_train, X_val, y_val, dnn_param, q)
+        y_pred = model.predict(test)
+        dnn_preds_test.append(np.ravel(y_pred))
+    tf.keras.backend.clear_session()
+
+# ensemble
+for i in range(10):
+    submission.loc[:,i] = rf_preds_test[i] * weight[0] + dnn_preds_test[i] * weight[1]
 
 #%% save prediction and validation result
 submission.to_csv(f'submit/submit_{save_num}.csv')
-val_preds_df.to_csv(f'val/val_{save_num}.csv')
+rf_preds_df.to_csv(f'val/rf_val_{save_num}.csv')
+dnn_preds_df.to_csv(f'val/dnn_val_{save_num}.csv')
 
 f = open(f"val/val_{save_num}.txt","w+")
 msg = f'history length:: {past_history} \n'
 f.write(msg)
 msg = f'used feature:: {x_col} \n'
 f.write(msg)
-msg = f'pinball cv loss:: {loss} \n'
+msg = f'pinball loss rf:: '+str(np.mean(val_results['rf']))+'\n'
+f.write(msg)
+msg = f'pinball loss dnn:: '+str(np.mean(val_results['dnn']))+'\n'
+f.write(msg)
+msg = f'pinball loss ens:: {np.mean(losses)} \n'
+f.write(msg)
+msg = f'used weight:: {weight} \n'
 f.write(msg)
 f.close()
